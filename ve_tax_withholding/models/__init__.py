0c9327b34519b4e88c30b7d7e75aee8e029796c3
omarquezpayall <omarquez@payall.com.ve>
2021-11-19 08:42:33 -0400

    feat: :sparkles: Modulos customizados

---
+++ ve_tax_withholding/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from . import tax_withholding_voucher_vendor
+from . import withholding_subject
+from . import account_move
+from . import tax_withholding_voucher_customer
---
+++ ve_tax_withholding/models/res_partner.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*- 
+
+from odoo import models, fields, api 
+
+class ResPartner(models.Model):
+    
+    _inherit = 'res.partner' 
+    
+    
+    partner_type_custom = fields.Selection([('PND', 'PND'),('PJD','PJD')], string='partner custom', store=True, )
+    
+    @api.onchange('is_company')
+    def _partner_type(self):
+        for partner in self:
+            if partner.is_company:
+                self.partner_type_custom = 'PJD'
+            else:
+                self.partner_type_custom = 'PND'
+            
+    
+
+            
+    
